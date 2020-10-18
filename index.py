91e8ff9a0d39cfa528b326c71023d603c9982895
giodp2408 <60194022+giodp2408@users.noreply.github.com>
2020-10-17 22:34:09 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,1 @@
- def application(environ, start_response):
-     # Guardo la salida que devolveré como respuesta
-     respuesta = "<html> <head> <title> Mi primera pagina web 1 </title> </head> <body style=\"background-color:00BB2D;\"> <h1 align=\"center\" >Mi Primera pagina web </h1> <hr> <p>Pagina de prueba HTML.</p> </body> </html>"
-     # Se genera una respuesta al navegador 
-     start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
-     return respuesta
+print("Hello World") 
